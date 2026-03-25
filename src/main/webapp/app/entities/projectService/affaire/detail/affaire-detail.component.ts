2496b1540983724fcbe9989d4aabfbeb52edf2ad
DaoudAnas13 <daoudanas28@gmail.com>
2026-03-25 09:43:47 +0100

    minor

--- src/main/webapp/app/entities/projectService/affaire/detail/affaire-detail.component.ts
+++ src/main/webapp/app/entities/projectService/affaire/detail/affaire-detail.component.ts
@@ -6,9 +6,13 @@ import { IAffaire } from '../affaire.model';
 @Component({
   selector: 'jhi-affaire-detail',
   templateUrl: './affaire-detail.component.html',
+  styleUrls: ['./affaire-detail.component.scss'],
 })
 export class AffaireDetailComponent implements OnInit {
   affaire: IAffaire | null = null;
+  articles: any[] = [];
+  matrice: any[] = [];
+  documents: any[] = [];
 
   constructor(protected activatedRoute: ActivatedRoute) {}
 
@@ -16,6 +20,18 @@ export class AffaireDetailComponent implements OnInit {
     this.activatedRoute.data.subscribe(({ affaire }) => {
       this.affaire = affaire;
     });
+
+    this.articles = [
+      { code: 'ART-001', designation: 'Câble fibre optique', unite: 'm', contractuelle: 50000, realisee: 42000 },
+      { code: 'ART-002', designation: 'Connecteur SC/APC', unite: 'pièce', contractuelle: 2000, realisee: 1750 },
+    ];
+
+    this.matrice = [
+      { ville: 'Tunis', zone: 'Nord', base: 50, nuit: 0 },
+      { ville: 'Sfax', zone: 'Centre', base: 55, nuit: 0 },
+    ];
+
+    this.documents = [{ name: 'etude_faisabilite.pdf' }, { name: 'cahier_charges.pdf' }, { name: 'bon_commande.pdf' }];
   }
 
   previousState(): void {
